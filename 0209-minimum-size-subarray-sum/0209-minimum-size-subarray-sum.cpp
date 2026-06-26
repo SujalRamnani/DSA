c7a33badd5761b468ecfeb4f50497a8893acbbf0
SujalRamnani <sujalramnani0@gmail.com>
2026-06-26 23:03:13 +0530

    Time: 0 ms (100%), Space: 41.8 MB (9.55%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& arr) {
         int n=arr.size();
-        int minLen=INT_MAX;
-        int low=0,high=0;
+        int low=0;int minLen=INT_MAX;
         int sum=0;
-        while(high<n){
-            sum=sum+arr[high];
+        for(int high=0;high<n;high++){
+            sum+=arr[high];
             while(sum>=target){
                 int len=high-low+1;
-                minLen=min(minLen,len);
-                //abb fire karte jao
-                sum=sum-arr[low];
+                minLen=min(len,minLen);
+                sum-=arr[low];
                 low++;
             }
-            high++;
 
         }
         if (minLen==INT_MAX) return 0;
         return minLen;
+        
     }
 };
