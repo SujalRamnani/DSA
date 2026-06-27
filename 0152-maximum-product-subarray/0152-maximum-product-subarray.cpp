0f2c616238ce8585bd45c6358c496dd980823bf2
SujalRamnani <sujalramnani0@gmail.com>
2026-06-27 10:01:24 +0530

    Time: 0 ms (100%), Space: 17.8 MB (59.3%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     int maxProduct(vector<int>& arr) {
+        // int n=arr.size();
+        // int bestMaxEnding=arr[0];
+        // int bestMinEnding=arr[0];
+        // int result=arr[0];
+        // for(int i=1;i<n;i++){
+        //     ///current i index ke pass 3 choice hai
+        //     int choice1=arr[i]; ///akela reh le
+        //     int choice2=arr[i]*bestMinEnding;    ////minEnding ke saath mil jaye
+        //      int choice3=arr[i]*bestMaxEnding;    ////maxEnding ke saath mil jaye
+
+        //      bestMinEnding=min(choice1,min(choice2,choice3));
+        //      bestMaxEnding=max(choice1,max(choice2,choice3));
+
+        //      ///result mein max pucha hai max product subarray
+        //      result=max(result,max(bestMinEnding,bestMaxEnding));
+        // }
+        // return result;
+
+
         int n=arr.size();
-        int bestMaxEnding=arr[0];
-        int bestMinEnding=arr[0];
         int result=arr[0];
+        int bestMinEnding=arr[0];
+        int bestMaxEnding=arr[0];
         for(int i=1;i<n;i++){
-            ///current i index ke pass 3 choice hai
-            int choice1=arr[i]; ///akela reh le
-            int choice2=arr[i]*bestMinEnding;    ////minEnding ke saath mil jaye
-             int choice3=arr[i]*bestMaxEnding;    ////maxEnding ke saath mil jaye
+            int choice1=arr[i];
+            int choice2=arr[i]*bestMinEnding;
+            int choice3=arr[i]*bestMaxEnding;
 
+            bestMaxEnding=max(choice1,max(choice2,choice3));
              bestMinEnding=min(choice1,min(choice2,choice3));
-             bestMaxEnding=max(choice1,max(choice2,choice3));
 
-             ///result mein max pucha hai max product subarray
-             result=max(result,max(bestMinEnding,bestMaxEnding));
+             result=max(result,max(bestMaxEnding,bestMinEnding));
+
         }
         return result;
-        
     }
 };
