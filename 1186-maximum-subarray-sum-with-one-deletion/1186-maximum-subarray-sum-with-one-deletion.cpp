5f8c333d0fe8caf91fef5dc8995be480e88d2dc8
SujalRamnani <sujalramnani0@gmail.com>
2026-07-01 09:30:23 +0530

    Time: 0 ms (100%), Space: 27.1 MB (95.63%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
+        ///we want maximum sum and we can ignore one element here//
+        //here we can use kadanes algorithm 
+        //at any index we are having 2 options 
+        //normally add karo no delete 
+        //ya fir delete karo ek element ko
+
         int n=arr.size();
         int noDelete=arr[0];
         int oneDelete=INT_MIN;
         int result=arr[0];
         for(int i=1;i<n;i++){
-            int prevNo = noDelete;
-int prevOne = oneDelete;
-
-noDelete = max(arr[i], prevNo + arr[i]);
-
-if(prevOne == INT_MIN)
-    oneDelete = prevNo;
-else
-    oneDelete = max(prevOne + arr[i], prevNo);
-
-result = max(result, max(noDelete, oneDelete));
-        }
-        return result;
+        int prevNo = noDelete;
+        int prevOne = oneDelete;
+       noDelete = max(arr[i], prevNo + arr[i]);
+      if(prevOne == INT_MIN) oneDelete = prevNo;
+      else oneDelete = max(prevOne + arr[i], prevNo);
+      result = max(result, max(noDelete, oneDelete));
+ }
+ return result;
     }
 };
