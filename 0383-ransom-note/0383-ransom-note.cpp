8d7507eb8dbe3df1eef6f3c855d0b1a3c22e7e97
SujalRamnani <sujalramnani0@gmail.com>
2026-07-01 18:21:02 +0530

    Time: 4 ms (39.95%), Space: 11.9 MB (28.95%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
     bool canConstruct(string a, string b) {
-        //kya string b se hum a ko bana sakte hai agar haan toh return true varna return false
-        unordered_map<char,int> mp;
-        for(int i=0;i<b.length();i++) mp[b[i]]++; 
+        // //kya string b se hum a ko bana sakte hai agar haan toh return true varna return false
+        // unordered_map<char,int> mp;
+        // for(int i=0;i<b.length();i++) mp[b[i]]++; 
     
-        for(int i=0;i<a.length();i++){
-            //har character b ka bs ek baar use karna hai agar string a ka character
-            //b mein nhi hai toh fir sidha false varna uske frequency ko ghate jao
-            if (mp[a[i]]==0) return false; 
-           else mp[a[i]]--;
+        // for(int i=0;i<a.length();i++){
+        //     //har character b ka bs ek baar use karna hai agar string a ka character
+        //     //b mein nhi hai toh fir sidha false varna uske frequency ko ghate jao
+        //     if (mp[a[i]]==0) return false; 
+        //    else mp[a[i]]--;
+        // }
+        // return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int m=a.length();int n=b.length();
+        unordered_map<char,int> mp;
+        for(int i=0;i<n;i++) mp[b[i]]++;
+
+        for(int i=0;i<m;i++){
+            if (mp.find(a[i])==mp.end()) return false;
+            mp[a[i]]--;
+            if (mp[a[i]]<0) return false; 
         }
         return true;
         
