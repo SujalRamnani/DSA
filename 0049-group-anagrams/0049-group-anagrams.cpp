05317a7715c623542ca3bb4d60153026172050dd
SujalRamnani <sujalramnani0@gmail.com>
2026-07-01 16:45:50 +0530

    Time: 20 ms (37.79%), Space: 26 MB (57.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& arr) {
-
         int n=arr.size();
-         vector<vector<string>> ans;
-        unordered_map<string,vector<string>> mp;
-        for(int i=0;i<n;i++){
+          vector<vector<string>> ans;
+          unordered_map<string,vector<string>> mp;
+          for(int i=0;i<n;i++){
             string temp=arr[i];
             sort(temp.begin(),temp.end());
-            mp[temp].push_back(arr[i]);
-        }
-        
-        for(auto it:mp) ans.push_back(it.second);
-        return ans;
+            mp[temp].push_back(arr[i]); //ek string ke corresponding vector uska sorted string ke corresponding
+          }
 
+          for(auto it:mp){
+            ans.push_back(it.second);
+          }
+          return ans;
     }
 };
