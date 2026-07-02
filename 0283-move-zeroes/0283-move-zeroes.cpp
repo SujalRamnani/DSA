dcc2355c7b1af9222956baa581ce951561bd00ac
SujalRamnani <sujalramnani0@gmail.com>
2026-07-02 10:46:01 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.81%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n=arr.size();
+        int i=-1;
+        for(int j=0;j<n;j++){
+            if (arr[j]==0){
+                i=j;
+                break;
+            }
+        }
+        if (i<0) return;
+        for(int j=i+1;j<n;j++){
+            if (arr[j]!=0){
+                swap(arr[j],arr[i]);
+                i++;
+            }
+        }
+        
+    }
+};
