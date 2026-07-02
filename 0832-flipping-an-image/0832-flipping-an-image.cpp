71996b3b71371fd8494843cabbfdaf8e074a3e00
SujalRamnani <sujalramnani0@gmail.com>
2026-07-02 12:27:03 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.22%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -2,9 +2,38 @@ class Solution {
 public:
 
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& arr) {
-        int m=arr.size();
+        // //har ek row ko reverse kardo
+        // int m=arr.size();
+        // int n=arr[0].size();
+        // for(int i=0;i<m;i++){
+        //     int a=0;int b=n-1;
+        //     while(a<b){
+        //         int temp=arr[i][a];
+        //         arr[i][a]=arr[i][b];
+        //         arr[i][b]=temp;
+        //         a++;
+        //         b--;
+        //     }
+        // }
+
+        // ///har ek row reverse hogye abb jaha pe zero hai usko 1 kardo aur jidhar 1 hai usko 0
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if (arr[i][j]==0) arr[i][j]=1;
+        //         else if (arr[i][j]==1) arr[i][j]=0;
+        //     }
+        // }
+        // return arr;
+
+int m=arr.size();
         int n=arr[0].size();
         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]^=1;
+            }
+        }
+  ///reverse kardo each row ko
+   for(int i=0;i<m;i++){
             int a=0;int b=n-1;
             while(a<b){
                 int temp=arr[i][a];
@@ -15,13 +44,6 @@ public:
             }
         }
 
-        ///har ek row reverse hogye abb jaha pe zero hai usko 1 kardo aur jidhar 1 hai usko 0
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (arr[i][j]==0) arr[i][j]=1;
-                else if (arr[i][j]==1) arr[i][j]=0;
-            }
-        }
         return arr;
         
     }
