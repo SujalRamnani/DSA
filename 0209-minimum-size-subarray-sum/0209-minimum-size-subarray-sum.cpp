2a3c13656255162101178b7c7d765856238815cf
SujalRamnani <sujalramnani0@gmail.com>
2026-07-03 18:15:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (20.57%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& arr) {
+        //isme subarray ka length nhi diyaa hai matlab variable size hai
         int n=arr.size();
-        int minLen=INT_MAX;
         int low=0,high=0;
+        int result=INT_MAX;
         int sum=0;
+        int minLen=INT_MAX;
         while(high<n){
             sum+=arr[high];
-            
-            while (sum>=target){
+            while(sum>=target){
                 int len=high-low+1;
                 minLen=min(minLen,len);
-                sum-=arr[low];
+                sum=sum-arr[low];
                 low++;
             }
-            
             high++;
-
         }
-       if (minLen==INT_MAX) return 0;
-       return minLen;
+        if (minLen==INT_MAX) return 0;
+        return minLen;
         
     }
 };
