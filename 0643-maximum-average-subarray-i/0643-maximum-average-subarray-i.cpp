632873b15580065130514c176b8c4dc766fb3c9b
SujalRamnani <sujalramnani0@gmail.com>
2026-07-04 15:11:45 +0530

    Time: 4 ms (25.13%), Space: 113.9 MB (27.42%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    double findMaxAverage(vector<int>& arr, int k) {
-        //fixed length ke window hai
-        int n=arr.size();
-        int low=0,high=k-1;
-        double avg=0;
-double maxAvg=0;int sum=0;
-        for(int i=low;i<=high;i++) sum+=arr[i];
-        avg=sum*1.0/k;
-maxAvg=avg;
+    double findMaxAverage(vector<int>& arr, int size) {
+        //subarray of k length maang raha hai matlab fixed size sliding window
 
+        ///jabhi fixed size length ka window ho tab first window ka nikalo aur fir slide karo
+        int low=0;int high=size-1;
+        int sum=0;int n=arr.size();
+        for(int i=low;i<=high;i++) sum+=arr[i];
+        double avg=(sum*1.0)/size; //1st window ka avg
+        double maxAvg=INT_MIN;
         while(high<n){
             maxAvg=max(maxAvg,avg);
+            //abb aage wali window matlab window ko slide karo
             low++;
             high++;
-            sum-=arr[low-1];
-             avg=sum*1.0/k;
             if (high==n) break;
-            sum+=arr[high];
-             avg=sum*1.0/k;
-           
-
-
+            sum=sum-arr[low-1];
+            sum=sum+arr[high];
+            avg=(sum*1.0)/size;
         }
         return maxAvg;
+
         
     }
 };
