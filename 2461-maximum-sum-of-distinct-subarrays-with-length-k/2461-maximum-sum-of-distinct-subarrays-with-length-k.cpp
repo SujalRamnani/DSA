0abc5a12bdd75886d406812befad4a55e9643094
SujalRamnani <sujalramnani0@gmail.com>
2026-07-05 09:33:45 +0530

    Time: 101 ms (60.48%), Space: 95.2 MB (70.96%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,60 +1,33 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& arr, int k) {
-        // int n=arr.size();
-        // int i=0,j=0;
-        // unordered_set<int> st;
-        // long long currWindowSum=0;
-        // long long maxSum=0;
-
-        // while(j<n){
-        //     while(st.count(arr[j])){
-        //         //set mein jab tak present hai tab tab aage badhte jao 
-        //         currWindowSum-=arr[i];
-        //         st.erase(arr[i]);
-        //         i++;
-        //     }
-        //      st.insert(arr[j]);
-        //     currWindowSum+=arr[j];
-           
-        //    if (j-i+1==k){
-        //     //window shrink karenge maxsum pehle bana lete hai
-        //     maxSum=max(maxSum,currWindowSum);
-        //     currWindowSum-=arr[i];
-        //     st.erase(arr[i]);
-        //     i++;
-            
-        //    }
-        //     j++;
-
-        // }
-        // return maxSum;
-
+     
+        //fixed size subarray length deke rakha hai fixed length sliding window
+        //aur elements disticnt ke liye map use kardenge
         int n=arr.size();
-        int low=0,high=k-1;long long sum=0;
+        int low=0,high=k-1;
         unordered_map<int,int> mp;
-        for(int i=low;i<=high;i++) {
-            sum+=arr[i];
-            mp[arr[i]]++;
-        }
-        long long maxSum=0;
-        if (mp.size()==k) maxSum=sum;
-        while(high<n){
-            sum-=arr[low];
+        for(int i=low;i<=high;i++) mp[arr[i]]++;
+
+        long long sum=0;
+        for(int i=low;i<=high;i++) sum+=arr[i];
+           long long maxSum=INT_MIN;
+ if(mp.size()==k) maxSum=max(maxSum,sum);
+           while(high<n){
             mp[arr[low]]--;
             if (mp[arr[low]]==0) mp.erase(arr[low]);
-
             low++;
             high++;
-            if (high==n) break;
 
-            sum+=arr[high];
-            mp[arr[high]]++;
-
-            if (mp.size()==k) {
-                maxSum=max(maxSum,sum);
+             if(high==n) break;
+             sum-=arr[low-1];
+            sum += arr[high];
+             mp[arr[high]]++;
+             if(mp.size()==k) maxSum=max(maxSum,sum);
             }
-        }
-return maxSum;
+
+           if (maxSum==INT_MIN) return 0;
+              return maxSum;
+        
     }
 };
