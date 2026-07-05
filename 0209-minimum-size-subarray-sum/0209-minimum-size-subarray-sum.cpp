15dae3bdba11dc31d591dab442ab0f64acfb1077
SujalRamnani <sujalramnani0@gmail.com>
2026-07-05 12:47:28 +0530

    Time: 1 ms (16.99%), Space: 42 MB (13.35%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& arr) {
-        //isme subarray ka length nhi diyaa hai matlab variable size hai
+        //variable length sliding window ka question hai
         int n=arr.size();
         int low=0,high=0;
-        int result=INT_MAX;
-        int sum=0;
         int minLen=INT_MAX;
+        int sum=0;
+       
         while(high<n){
             sum+=arr[high];
+          
             while(sum>=target){
                 int len=high-low+1;
-                minLen=min(minLen,len);
-                sum=sum-arr[low];
-                low++;
+                minLen=min(len,minLen);
+               sum-=arr[low];
+                  low++;
+               
             }
             high++;
+
         }
         if (minLen==INT_MAX) return 0;
         return minLen;
