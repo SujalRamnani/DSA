da3257ccb028258e09d57cb1384a982397c382c9
SujalRamnani <sujalramnani0@gmail.com>
2026-07-05 13:49:15 +0530

    Time: 49 ms (33.38%), Space: 85 MB (33.99%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int totalFruit(vector<int>& arr) {
-               
-            ///max fruits in k baskets where k=2
-            int n=arr.size();
-            int maxi=0;
-            int low=0;
-            if (n==1) return 1;
-            if (n==2) return 2;
-            unordered_map<int,int> mp;
-            for(int high=0;high<n;high++){
-                mp[arr[high]]++;
+        //find longest subarray that has atmost 2 distinct elements
+       
+         unordered_map<int,int> mp;
+        int low=0,high=0;
+       
+        int maxLen=0;int n=arr.size();
+      
 
-                while(mp.size()>2){
-                    mp[arr[low]]--;
-                    if (mp[arr[low]]==0) mp.erase(arr[low]);
-                    low++;
-                }
-                if (mp.size()<=2){
-                    int len=high-low+1;
-                    maxi=max(maxi,len);
-                }
-            }   
-return maxi;
-
+        while(high<n){
+            mp[arr[high]]++; 
+            //jab tak information galat hai tab tak low ko aage badhao window ko shrink karo
+            //aur information ko thik karne ka try karo
+            
+            ///galat info kya hoga isme toh k distinct character wla window sahi hoga
+            //toh map ka size agar k hai toh thik hai agar k se jyada hai toh vo galat information ahi
+            while(mp.size()>2){
+                mp[arr[low]]--;
+                if (mp[arr[low]]==0) mp.erase(arr[low]);
+                low++;
+            } //loop tuta abb mp ka size k hai matlab information sahi hai
+            if(mp.size()<=2){
+                int len=high-low+1;
+                maxLen = max(maxLen,len);
+            }
+            high++;
+        }
+          
+        return maxLen;
+        
     }
 };
