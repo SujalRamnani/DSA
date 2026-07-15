6a10e2c47763c5a4c0a3b92a1e9c9512e246fd5b
SujalRamnani <sujalramnani0@gmail.com>
2026-07-15 13:05:56 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.12%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,27 +1,21 @@
+
 class Solution {
 public:
-    int result = 0;
+int ans=0;
+void solve(TreeNode* root,int sum){
+    if (root==NULL) return ;
+    sum=sum*10+root->val;
+    if (root->left==NULL && root->right==NULL){
+        //matlab apan leaf node pe aagye
+        ans+=sum;
 
-    void solve(TreeNode* root, int sum) {
-
-        if (root == NULL)
-            return;
-
-        sum = sum * 10 + root->val;
-
-        if (root->left == NULL && root->right == NULL) {
-            result += sum;
-            return;
-        }
-
-        solve(root->left, sum);
-        solve(root->right, sum);
     }
-
+    solve(root->left,sum);
+    solve(root->right,sum);
+}
     int sumNumbers(TreeNode* root) {
-
-        solve(root, 0);
-
-        return result;
+        solve(root,0);
+        return ans;
+        
     }
 };
