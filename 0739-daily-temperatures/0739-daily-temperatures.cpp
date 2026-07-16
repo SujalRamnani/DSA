e81e69b0941eb249f6f188657f8ec2e5fe6b8161
SujalRamnani <sujalramnani0@gmail.com>
2026-07-16 15:51:02 +0530

    Time: 27 ms (34.08%), Space: 102.9 MB (78.91%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int> &arr) {
-         int n=arr.size();
+    vector<int> dailyTemperatures(vector<int>& arr) {
+        int n=arr.size();
         stack<int> st;
         vector<int> ans(n);
         ans[n-1]=0;
         st.push(n-1);
         for(int i=n-2;i>=0;i--){
-            while(st.size()!=0 && arr[st.top()]<=arr[i]) st.pop();
-            if (st.size()==0) ans[i]=0;
-            else{
-                ans[i]=st.top()-i;
-            }
+            //pehle pop fir ans mein add fir st mein push
+            while(st.size()!=0 && arr[i]>=arr[st.top()]) st.pop();
+
+            if (st.size()==0 ) ans[i]=0;
+            else ans[i]=st.top()-i;
+            
             st.push(i);
+
         }
         return ans;
+        
     }
 };
