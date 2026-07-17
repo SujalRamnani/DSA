4add4f84700b8df9e275e2ce9f0566840045f911
SujalRamnani <sujalramnani0@gmail.com>
2026-07-17 17:15:20 +0530

    Time: 15 ms (30.96%), Space: 14 MB (79.86%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        int n=nums.size();
-        int closestSum=100000;
-sort(nums.begin(),nums.end());
-        for(int k=0;k<n;k++){
-            int i=k+1,j=n-1;
+    int threeSumClosest(vector<int>& arr, int target) {
+        int close=100000;
+        sort(arr.begin(), arr.end());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int n1=arr[i];
+            int p=i+1;int q=n-1;
+            while(p<q){
+                int sum=arr[p]+arr[q]+n1;
+                if (abs(sum-target)<abs(target-close)) close=sum; ///taget-close bada hai apne ko chota close chahiye isliye usko update karte jao
+                else if (sum>target) q--;
+                else p++;
 
-            while(i<j){
-            int sum=nums[k]+nums[i]+nums[j];
-
-            if (abs(target-sum)<abs(target-closestSum)) closestSum=sum;
-            if (sum<target) i++;
-            else j--;
             }
+
         }
-        return closestSum;
+        return close;
+        
     }
 };
