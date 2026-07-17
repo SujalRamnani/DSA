9decc89d677be96cc8f00019f4fe52f84421c098
SujalRamnani <sujalramnani0@gmail.com>
2026-07-17 11:23:10 +0530

    Time: 3 ms (24.58%), Space: 8.9 MB (66.36%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n=s.length();
+        ///ek letter max ek part mein ana chahiye
+        unordered_map<char,int> mp;//ye last index tak store karega
+        for(int i=0;i<n;i++) mp[s[i]]=i;
         vector<int> ans;
-        unordered_map<char,int> mp;
- for(int i=0;i<n;i++) mp[s[i]]=i; //map chahiye jo last occurence store kare na ki frequency
-
         int i=0;
         while(i<n){
-          int end=mp[s[i]];
+            int end=mp[s[i]];  ///current element ka end point tak nikaleaga
+            ///ye end jo hai vo expand hota rahega
             int j=i;
             while(j<end){
                 end=max(end,mp[s[j]]);
@@ -17,7 +18,7 @@ public:
             ans.push_back(j-i+1);
             i=j+1;
         }
-return ans;
+        return ans;
         
     }
 };
