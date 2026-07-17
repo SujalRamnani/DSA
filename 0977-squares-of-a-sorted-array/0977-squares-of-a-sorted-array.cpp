736f06d296585f655106aca76cf99b0753e1f32c
SujalRamnani <sujalramnani0@gmail.com>
2026-07-17 14:29:48 +0530

    Time: 14 ms (7.02%), Space: 30.4 MB (39.86%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,50 +1,10 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& arr) {
-        // int n=arr.size();
-        // for(int i=0;i<n;i++) arr[i]*=arr[i];
-        // sort(arr.begin(),arr.end());
-        // return arr;
-
-
-      
-        vector<int> neg;
-        vector<int> pos;
-        for(int i=0;i<arr.size();i++) {
-            if (arr[i]<0) neg.push_back(arr[i]);
-            else pos.push_back(arr[i]);
-        }
-        
-        for(int i=0;i<neg.size();i++) neg[i]=neg[i]*neg[i];sort(neg.begin(),neg.end());
-          for(int i=0;i<pos.size();i++) pos[i]=pos[i]*pos[i];
-
-          int m=neg.size();int n=pos.size();
-int i=0,j=0;vector<int> ans;
-while(i<m && j<n){
-    if (neg[i]<=pos[j]){
-        ans.push_back(neg[i]);
-        i++;
-
-    }
-    else{
-         ans.push_back(pos[j]);
-        j++;
-
-    }
-}
-
-while(j<n){
-      ans.push_back(pos[j]);
-        j++;
-
-}
-while(i<m){
-   ans.push_back(neg[i]);
-        i++;  
-}
-return ans;
-        
-
+        int n=arr.size();
+        for(int i=0;i<n;i++) arr[i]*=arr[i];
+        sort(arr.begin(),arr.end());
+        return arr;
         
     }
 };
