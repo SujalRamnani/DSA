16b5c65698995ea15adfeee45e9eab1e1c9c8551
SujalRamnani <sujalramnani0@gmail.com>
2026-07-17 16:38:44 +0530

    Time: 50 ms (47.1%), Space: 33.2 MB (13.86%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-vector<vector<int>> result;
-void twoSum(vector<int>& arr,int target,int i,int j){
+vector<vector<int>> ans;
+void twoSum(vector<int> &arr,int target,int i,int j){
     while(i<j){
-        if (arr[i]+arr[j]>target) j--;
-        else if (arr[i]+arr[j]<target) i++;
+
+        if (arr[i]+arr[j]<target) i++;
+        else if (arr[i]+arr[j]>target) j--;
         else{
-            //first remove duplicates
-        while (i<j && arr[i]==arr[i+1]) i++;
-        while(i<j && arr[j]==arr[j-1]) j--;
-        
-        result.push_back({-target,arr[i],arr[j]});
-        i++;
-        j--;
-
-
+            //pehle remove duplicates
+            while(i<j && arr[i]==arr[i+1]) i++;
+            while(i<j && arr[j]==arr[j-1]) j--;
+            ans.push_back({-target,arr[i],arr[j]});
+            i++;
+            j--;
         }
     }
 }
     vector<vector<int>> threeSum(vector<int>& arr) {
-        ////triplets return karna hai matlab clearly two pointer ka hai
-        int n=arr.size();
-        ///sort karne pe fayda hoga yes hoga toh kardo
+        //sort karne pe fayda horaha hai toh kardo
         sort(arr.begin(),arr.end());
+        int n=arr.size();
         for(int i=0;i<n;i++){
-            if (i>0 && arr[i]==arr[i-1]) continue;
+            if (i>0 && arr[i]==arr[i-1]) continue; //pehle agar aa chuka hai as n1 toh firse nhi lo
             int n1=arr[i];
             int target=-n1;
             twoSum(arr,target,i+1,n-1);
         }
-        return result;
+        return ans;
+
+
         
     }
 };
