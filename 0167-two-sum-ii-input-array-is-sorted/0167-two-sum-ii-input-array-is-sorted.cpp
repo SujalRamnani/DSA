afa34329c5585497432027d0e7943710f981c093
SujalRamnani <sujalramnani0@gmail.com>
2026-07-17 11:43:20 +0530

    Time: 0 ms (100%), Space: 19.6 MB (34.01%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
         int n=arr.size();
+        vector<int> ans;
         unordered_map<int,int> mp;
-        
         for(int i=0;i<n;i++){
-           
             int remaining=target-arr[i];
             if (mp.find(remaining)!=mp.end()) return {mp[remaining]+1,i+1};
             mp[arr[i]]=i;
