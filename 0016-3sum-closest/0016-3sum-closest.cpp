bc0a412685a1a03e951d96024fa14016731778ac
SujalRamnani <sujalramnani0@gmail.com>
2026-07-18 08:35:31 +0530

    Time: 18 ms (15.03%), Space: 14.2 MB (11.17%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& arr, int target) {
         int close=100000;
-        sort(arr.begin(), arr.end());
+        sort(arr.begin(), arr.end()); //sort karne pe fayda hai aur index bhi nhi manga kardo
         int n=arr.size();
         for(int i=0;i<n;i++){
             int n1=arr[i];
