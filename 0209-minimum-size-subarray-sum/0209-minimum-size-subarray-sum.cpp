ba786f5f49cd2fbc5230e849b4de57d1b96399d8
SujalRamnani <sujalramnani0@gmail.com>
2026-07-18 23:49:26 +0530

    Time: 4 ms (7.95%), Space: 42 MB (22.8%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& arr) {
-        //variable length sliding window ka question hai
         int n=arr.size();
         int low=0,high=0;
         int minLen=INT_MAX;
         int sum=0;
-       
         while(high<n){
             sum+=arr[high];
-          
             while(sum>=target){
                 int len=high-low+1;
-                minLen=min(len,minLen);
-               sum-=arr[low];
-                  low++;
-               
+            minLen=min(len,minLen);
+                sum-=arr[low];
+                low++;
             }
+            
             high++;
-
         }
         if (minLen==INT_MAX) return 0;
         return minLen;
