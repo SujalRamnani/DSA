5247a601bb5b0036c4db15e37b46e14b17c8c724
SujalRamnani <sujalramnani0@gmail.com>
2026-07-18 09:05:26 +0530

    Time: 29 ms (42.24%), Space: 16.2 MB (80.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -4,13 +4,12 @@ void dfs(vector<vector<char>>& arr,int i,int j){
      int m=arr.size();
         int n=arr[0].size();
         if (i<0 || i>=m || j<0 || j>=n || arr[i][j]!='1') return;
-        if (arr[i][j]==-1) return;
 
-        arr[i][j]='$';
-        dfs(arr,i-1,j);
-        dfs(arr,i+1,j);
-        dfs(arr,i,j-1);
-        dfs(arr,i,j+1);
+        arr[i][j]='$'; ///visited
+        dfs(arr,i-1,j); //up
+        dfs(arr,i+1,j); //down
+        dfs(arr,i,j-1); //left
+        dfs(arr,i,j+1); //right
 
 }
     int numIslands(vector<vector<char>>& arr) {
