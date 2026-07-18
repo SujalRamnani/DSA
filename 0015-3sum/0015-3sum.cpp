7c4738bcbeede9084a4524c97048f5ef5cb45d5c
SujalRamnani <sujalramnani0@gmail.com>
2026-07-18 08:29:18 +0530

    Time: 51 ms (43.47%), Space: 33.3 MB (13.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-vector<vector<int>> ans;
-void twoSum(vector<int> &arr,int target,int i,int j){
+  vector<vector<int>> ans;
+  void twoSum(vector<int>& arr,int target,int i,int j){
     while(i<j){
-
         if (arr[i]+arr[j]<target) i++;
         else if (arr[i]+arr[j]>target) j--;
         else{
-            //pehle remove duplicates
+            ///pehle duplicates ko hatao
             while(i<j && arr[i]==arr[i+1]) i++;
             while(i<j && arr[j]==arr[j-1]) j--;
+
             ans.push_back({-target,arr[i],arr[j]});
             i++;
             j--;
         }
     }
-}
+  }
     vector<vector<int>> threeSum(vector<int>& arr) {
-        //sort karne pe fayda horaha hai toh kardo
-        sort(arr.begin(),arr.end());
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            if (i>0 && arr[i]==arr[i-1]) continue; //pehle agar aa chuka hai as n1 toh firse nhi lo
-            int n1=arr[i];
-            int target=-n1;
-            twoSum(arr,target,i+1,n-1);
-        }
-        return ans;
-
-
-        
+int n=arr.size();
+   //triplet maang raha hai two pointer use hoga aur sort karne mein fayda bhi hai
+   sort(arr.begin(),arr.end());
+   for(int i=0;i<n;i++){
+    if (i>0 && arr[i]==arr[i-1]) continue; //ek baar liya hua n1 firse wahi number nhi le sakte
+    int n1=arr[i];
+    int target=-n1;
+    twoSum(arr,target,i+1,n-1);
+   } 
+   return ans;       
     }
 };
