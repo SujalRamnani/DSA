35c63e1cd0afd0caa0e713118c0e091cafb1f173
SujalRamnani <sujalramnani0@gmail.com>
2026-07-19 23:20:52 +0530

    Time: 0 ms (100%), Space: 11.2 MB (39.65%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -11,35 +11,35 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (head==NULL) return head;
-        ListNode* temp=head;
-       
-        ListNode* before=NULL;
+        if (head==NULL || head->next==NULL) return head;
         int pos=1;
+        ListNode* temp=head;
+        ListNode* before=NULL;
         while(pos<left){
             before=temp;
             temp=temp->next;
             pos++;
-        }
 
-      
+        }
+        ///abb temp aagya left pe
+        //before usse pehle hai 
         ListNode* curr=temp;
         ListNode* prev=NULL;
-      int times=right-left+1;
+        int times=right-left+1;
         while(times--){
             ListNode* next=curr->next;
             curr->next=prev;
             prev=curr;
             curr=next;
-
         }
-        temp->next=curr;
-        if (before) {
-            before->next=prev;
-            return head;
-        }
+        ///abb curr aage poch gya 5 pe
+         temp->next=curr;
+       if (before){
+         before->next=prev;
+         return head;
+       }
+       
         return prev;
         
-
     }
 };
