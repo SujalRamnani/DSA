4828b61bcae22f302e89704a99fe8f2222a412af
SujalRamnani <sujalramnani0@gmail.com>
2026-07-20 19:30:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,11 +4,13 @@ unordered_map<int,int> dp;
     int fib(int n) {
         if (n==0) return 0;
         if (n==1) return 1;
-        if (dp.find(n)!=dp.end()) return dp[n];
+        if (dp.find(n)!=dp.end()) return dp[n]; //kahi ye mere dp mein hai toh nhi pehle se
+        ///agar hai toh return kardo///
         int a1=fib(n-1);
         int a2=fib(n-2);
         int ans=a1+a2;
-        dp[n]=ans;
+        dp[n]=ans; //nhi hai 1st time solve horaha hai usko dp mein daaldo..isko yaad karlo
+        ///aage kabhi ayega toh return karenge////
         return ans;
 
         
