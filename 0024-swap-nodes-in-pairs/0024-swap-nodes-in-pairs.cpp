154c0fa089de7a097d365826ceb05f7c3e8dc61c
SujalRamnani <sujalramnani0@gmail.com>
2026-07-20 11:08:30 +0530

    Time: 0 ms (100%), Space: 11.2 MB (20.37%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,48 @@
+
+class Solution {
+public:
+void reverse(ListNode* head,int times){
+    ListNode* curr=head;
+        ListNode* prev=NULL;
+        while(times--){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+}
+    ListNode* swapPairs(ListNode* head) {
+        if (head==NULL) return NULL;
+        ListNode* left=head;
+        ListNode* right;
+        ListNode* prevLeft=NULL;
+        int size=2;
+        ListNode* res=NULL;
+        while(true){
+            right=left;
+            for(int i=0;i<(size-1);i++){
+                if (right==NULL) break;
+                 right=right->next;
+            }
+            if (right){
+                ListNode* nextLeft=right->next;
+                reverse(left,2);
+                if (prevLeft)   prevLeft->next=right;
+                prevLeft=left;
+                if (res==NULL) res=right;
+                left=nextLeft;  
+            }
+
+            else{
+                //khatm sab
+                if (prevLeft) prevLeft->next=left;
+                if (res==NULL) res=left;
+                break;
+            }
+
+        }
+        return res;
+        
+    }
+};
