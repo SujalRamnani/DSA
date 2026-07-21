ee8e4f0a89bc74e0da50c07543f4ccbbe1fd635f
SujalRamnani <sujalramnani0@gmail.com>
2026-07-21 12:29:20 +0530

    Time: 3 ms (75.5%), Space: 18.7 MB (26.04%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n=s.length();
+        int i=0,j=n-1;
+        while(i<j){
+            if (s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else if (s[i]!=s[j]){
+                char mini=min(s[i],s[j]);
+                s[i]=mini; //s[mini] ye index matlab ascii value dega aapan s[i] mein mini put kar rahe hai
+                s[j]=mini;
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
