9636a55edc8f54104e3cbcfe0ffa1dbcde516088
SujalRamnani <sujalramnani0@gmail.com>
2026-07-23 09:29:07 +0530

    Time: 11 ms (8.4%), Space: 11.7 MB (75.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     bool canConstruct(string a, string b) {
-        // //kya string b se hum a ko bana sakte hai agar haan toh return true varna return false
-        // unordered_map<char,int> mp;
-        // for(int i=0;i<b.length();i++) mp[b[i]]++; 
-    
-        // for(int i=0;i<a.length();i++){
-        //     //har character b ka bs ek baar use karna hai agar string a ka character
-        //     //b mein nhi hai toh fir sidha false varna uske frequency ko ghate jao
-        //     if (mp[a[i]]==0) return false; 
-        //    else mp[a[i]]--;
-        // }
-        // return true;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         int m=a.length();int n=b.length();
         unordered_map<char,int> mp;
