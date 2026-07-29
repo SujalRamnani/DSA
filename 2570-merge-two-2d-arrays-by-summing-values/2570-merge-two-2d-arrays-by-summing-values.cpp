eb9f5f689b453deab9729b22d9540b2451bf5bda
SujalRamnani <sujalramnani0@gmail.com>
2026-07-29 22:07:14 +0530

    Time: 2 ms (30.95%), Space: 15.1 MB (17.03%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,34 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& a, vector<vector<int>>& b) {
-      int i=0,j=0;
-      int m=a.size();
-      int n=b.size();
-      vector<vector<int>>  ans;
-      while(i<m && j<n){
-        if (a[i][0]<b[j][0]){
-            ans.push_back(a[i]);
-            i++;
+    //   int i=0,j=0;
+    //   int m=a.size();
+    //   int n=b.size();
+    //   vector<vector<int>>  ans;
+    //   while(i<m && j<n){
+    //     if (a[i][0]<b[j][0]){
+    //         ans.push_back(a[i]);
+    //         i++;
 
-        }
-        else if (b[j][0]<a[i][0]){
-            ans.push_back(b[j]);
-            j++;
-        }
-        else{
-            ans.push_back({a[i][0],a[i][1]+b[j][1]});
-            i++;
-            j++;
-        }
-      } 
-      while(i<m) {
-         ans.push_back(a[i]);
-            i++;
-      } 
-      while(j<n){
-          ans.push_back(b[j]);
-            j++;
-      }
-       return ans; 
+    //     }
+    //     else if (b[j][0]<a[i][0]){
+    //         ans.push_back(b[j]);
+    //         j++;
+    //     }
+    //     else{
+    //         ans.push_back({a[i][0],a[i][1]+b[j][1]});
+    //         i++;
+    //         j++;
+    //     }
+    //   } 
+    //   while(i<m) {
+    //      ans.push_back(a[i]);
+    //         i++;
+    //   } 
+    //   while(j<n){
+    //       ans.push_back(b[j]);
+    //         j++;
+    //   }
+    //    return ans; 
+
+vector<vector<int>>  ans;
+    unordered_map<int,int> mp;
+    int m=a.size();
+      int n=b.size();
+    for(int i=0;i<m;i++){
+        int id=a[i][0];
+        int value=a[i][1];
+
+        mp[id]+=value;
+    }
+
+    for(int i=0;i<n;i++){
+         int id=b[i][0];
+        int value=b[i][1];
+
+        mp[id]+=value;
+    }
+    for(auto it:mp){
+        int key=it.first;
+        int value=it.second;
+
+        ans.push_back({key,value});
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
     }
 };
