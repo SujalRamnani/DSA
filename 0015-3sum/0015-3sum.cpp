f6556e05ed19bba06b03e75f734b04b4cfe25e0c
SujalRamnani <sujalramnani0@gmail.com>
2026-08-14 11:40:53 +0530

    Time: 55 ms (27.41%), Space: 33 MB (13.58%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-  vector<vector<int>> ans;
-  void twoSum(vector<int>& arr,int target,int i,int j){
+ vector<vector<int>> ans;
+void twoSum(vector<int>& arr,int i,int j,int target){
+    int n=arr.size();
     while(i<j){
-        if (arr[i]+arr[j]<target) i++;
-        else if (arr[i]+arr[j]>target) j--;
+        if (arr[i]+arr[j]>target) j--;
+        else if (arr[i]+arr[j]<target) i++;
         else{
-            ///pehle duplicates ko hatao
+            //pehle remove duplicates
             while(i<j && arr[i]==arr[i+1]) i++;
             while(i<j && arr[j]==arr[j-1]) j--;
 
@@ -15,17 +16,21 @@ public:
             j--;
         }
     }
-  }
+}
     vector<vector<int>> threeSum(vector<int>& arr) {
-int n=arr.size();
-   //triplet maang raha hai two pointer use hoga aur sort karne mein fayda bhi hai
-   sort(arr.begin(),arr.end());
-   for(int i=0;i<n;i++){
-    if (i>0 && arr[i]==arr[i-1]) continue; //ek baar liya hua n1 firse wahi number nhi le sakte
-    int n1=arr[i];
-    int target=-n1;
-    twoSum(arr,target,i+1,n-1);
-   } 
-   return ans;       
+       
+        ////triplets return karna hai so 2 pointer pattern 
+        //sort karne pe bhi ffayda hai 
+          int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if (i>0 && arr[i]==arr[i-1]) continue;
+            int n1=arr[i];
+            int target=-n1;  ///n2+n3=-n1//  //n2+n3=-target//
+
+            twoSum(arr,i+1,n-1,target);
+        }
+        return ans;
+        
     }
 };
