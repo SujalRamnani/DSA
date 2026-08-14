8373ddf9c5f52e5b6ef249c160eb096024c2d1fe
SujalRamnani <sujalramnani0@gmail.com>
2026-08-14 12:37:49 +0530

    Time: 19 ms (12.61%), Space: 14.1 MB (15.05%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& arr, int target) {
+        //3 no ko jodd ke pata karna hai konsa target se sabse nazdik hai//
         int n=arr.size();
-        //sort karne pe fayda hai aur index nhi manga toh kardo
+          //sort karne pe fayda hai aur index nhi manga toh kardo
         sort(arr.begin(),arr.end());
-        int close=100000;
+        int closest=100000;
         for(int i=0;i<n;i++){
             int n1=arr[i];
-            int p=i+1,q=n-1;
+            int p=i+1;int q=n-1;
             while(p<q){
                 int sum=n1+arr[p]+arr[q];
-                if (abs(sum-target)<abs(close-target)) close=sum;
-                else if (sum>target) q--;
-                else p++;
+                  if (abs(target-sum)<abs(target-closest))   closest=sum;     
+               if (sum<target) p++;
+                else q--;
             }
         }
-        return close;
+        return closest;
+        
     }
 };
