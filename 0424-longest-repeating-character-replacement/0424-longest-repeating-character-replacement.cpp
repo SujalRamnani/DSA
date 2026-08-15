441100eb187eca119f42461f40911e6638924bbc
SujalRamnani <sujalramnani0@gmail.com>
2026-08-15 14:24:34 +0530

    Time: 193 ms (5.27%), Space: 11 MB (42.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
+int maxCount(vector<int> &f){
+    int maxi=INT_MIN;
+    for(int i=0;i<f.size();i++) maxi=max(maxi,f[i]);
+    return maxi;
+}
     int characterReplacement(string s, int k) {
         int n=s.length();
-        unordered_map<char,int> mp;
         int low=0,high=0;
-        int maxLen=0;
+        int maxLen=INT_MIN;
+        vector<int> f(256,0);
         while(high<n){
-            mp[s[high]]++;
-            ////jab tak information galat hai tab tak low ko aage badhao
-           ///k replacement karne ke baad window ke sare character same hai ya nhi batao
-            int maxFreq = 0;
-             // 2. Find maximum frequency in current window
-            for(auto it : mp)  maxFreq = max(maxFreq, it.second);
+            f[s[high]]++;
             int len=high-low+1;
-             // 3. Window invalid?
-            while(len-maxFreq>k){
-                mp[s[low]]--;
-                if(mp[s[low]] == 0)  mp.erase(s[low]);
-                low++;
-                ///length change hogi firse nikalo aur frequency bhi change hogi
-                len=high-low+1;
-                maxFreq=0;
-                 for(auto it : mp)  maxFreq = max(maxFreq, it.second);
+            int count=maxCount(f);
+            int diff=len-count;
 
-                
+            while(diff>k){
+                f[s[low]]--;
+                low++;
+                count=maxCount(f);
+                len=high-low+1;
+                diff=len-count;
             }
-             len=high-low+1;
-            maxLen=max(maxLen,len);
+            if (diff<=k) maxLen=max(maxLen,high-low+1);
             high++;
+
         }
         return maxLen;
         
