1f7cb7e5e3c2dd7e9d46a1e37ab0746f2d860b81
SujalRamnani <sujalramnani0@gmail.com>
2026-08-21 12:48:10 +0530

    Time: 19 ms (23.33%), Space: 11.4 MB (65.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-bool sahiHai(vector<int> &have,vector<int> &needed){
-    for(int i=0;i<256;i++){
-        if (have[i]<needed[i]) return false;
-    }
-    return true;
-}
     string minWindow(string s, string t) {
         int n=s.length();
         int m=t.length();
+        unordered_map<char,int> mp1;
+        for(int i=0;i<t.length();i++) mp1[t[i]]++;
 
-        vector<int> needed(256,0);
-        for(int i=0;i<t.length();i++) needed[t[i]]++;
-
-        vector<int> have(256,0);
-        
-        int res=INT_MAX;
+        ///variable length sw ka hai sawal
+        int minLen=INT_MAX;
         int low=0,high=0;
-        int start=-1;
+        int count=0;int start=0;
+        unordered_map<char,int> mp2;
         while(high<n){
-            have[s[high]]++;
-
-            ///jab tak info sahi hai tab tak 
-            while(sahiHai(have,needed)){
-                  int len=high-low+1;
-                  if (res>len){
-                    res=len;
-                    start=low;
-                  }
-                  have[s[low]]--;
-                  low++;
+           mp2[s[high]]++;
+           if (mp1.find(s[high])!=mp1.end() && mp2[s[high]]<=mp1[s[high]]) count++;
+           while(count==t.length()){
+            int len=high-low+1;
+            if (minLen>len){
+                minLen=len;
+                start=low;
             }
-            high++;
+            mp2[s[low]]--;
+            if(mp1.find(s[low])!=mp1.end() && mp2[s[low]]<mp1[s[low]]) count--;
+            low++;
+           }
+
+           high++;
         }
-        if (res==INT_MAX) return "";
-        return s.substr(start,res);  
+        if (minLen==INT_MAX) return "";
+        return s.substr(start,minLen);
     }
 };
