7047ecec80afb5d8abe68818ca81fa91f7378802
SujalRamnani <sujalramnani0@gmail.com>
2026-08-21 17:34:58 +0530

    Time: 7 ms (61.46%), Space: 11.6 MB (43.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<t.length();i++) mp[t[i]]++;
+
         int n=s.length();
-        int m=t.length();
-        unordered_map<char,int> mp1;
-        for(int i=0;i<t.length();i++) mp1[t[i]]++;
-
-        ///variable length sw ka hai sawal
-        int minLen=INT_MAX;
         int low=0,high=0;
-        int count=0;int start=0;
-        unordered_map<char,int> mp2;
-        while(high<n){
-           mp2[s[high]]++;
-           if (mp1.find(s[high])!=mp1.end() && mp2[s[high]]<=mp1[s[high]]) count++;
-           while(count==t.length()){
-            int len=high-low+1;
-            if (minLen>len){
-                minLen=len;
-                start=low;
-            }
-            mp2[s[low]]--;
-            if(mp1.find(s[low])!=mp1.end() && mp2[s[low]]<mp1[s[low]]) count--;
-            low++;
-           }
+        int minLen=INT_MAX;
+        int requiredCount=t.length();
+        int start=0;
 
-           high++;
+        while(high<n){
+            char ch=s[high];
+            if (mp[ch]>0) requiredCount--;
+            mp[ch]--;
+
+            while (requiredCount==0){
+                ///start shrinking///
+                int len=high-low+1;
+                if (minLen>len){
+                    minLen=len;
+                    start=low;
+                }
+                mp[s[low]]++;
+                if (mp[s[low]]>0){
+                requiredCount++;
+                }
+                low++;
+            }
+
+            high++;
         }
         if (minLen==INT_MAX) return "";
         return s.substr(start,minLen);
