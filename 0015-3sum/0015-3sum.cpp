31864e83fe6b936b6931dc90f581d21f12e5dcd0
SujalRamnani <sujalramnani0@gmail.com>
2026-09-03 08:52:06 +0530

    Time: 46 ms (66.9%), Space: 35 MB (11.97%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
- vector<vector<int>> ans;
-void twoSum(vector<int>& arr,int i,int j,int target){
-    int n=arr.size();
+   vector<vector<int>> ans;
+   void twoSum(vector<int>& arr,int target,int i,int j){
     while(i<j){
-        if (arr[i]+arr[j]>target) j--;
-        else if (arr[i]+arr[j]<target) i++;
+        int sum=arr[i]+arr[j];
+        if (sum>target) j--;
+        else if (sum<target) i++;
         else{
-            //pehle remove duplicates
-            while(i<j && arr[i]==arr[i+1]) i++;
-            while(i<j && arr[j]==arr[j-1]) j--;
+           while(i<j && arr[i]==arr[i+1]) i++;
+           while(i<j && arr[j]==arr[j-1]) j--;
 
-            ans.push_back({-target,arr[i],arr[j]});
-            i++;
-            j--;
+           vector<int> temp={-target,arr[i],arr[j]};
+           ans.push_back(temp);
+
+           i++;
+           j--;
         }
     }
-}
+   }
     vector<vector<int>> threeSum(vector<int>& arr) {
-       
-        ////triplets return karna hai so 2 pointer pattern 
-        //sort karne pe bhi ffayda hai 
-          int n=arr.size();
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            if (i>0 && arr[i]==arr[i-1]) continue;
-            int n1=arr[i];
-            int target=-n1;  ///n2+n3=-n1//  //n2+n3=-target//
+      
+         int n=arr.size();
+         ////index return nhi karna isliye sort kardo fayda hai
+         sort(arr.begin(),arr.end());
 
-            twoSum(arr,i+1,n-1,target);
-        }
-        return ans;
-        
+         for(int i=0;i<n;i++){
+            if (i>0 && arr[i]==arr[i-1]) continue; //skip kardunga agar pehle aagya toh
+            int n1=arr[i];
+            int target=-n1;
+            twoSum(arr,target,i+1,n-1);
+         }
+return ans;
     }
 };
