ce7ef386e0181755327142c84d07611f1011bf59
SujalRamnani <sujalramnani0@gmail.com>
2026-09-05 09:07:20 +0530

    Time: 3 ms (25.93%), Space: 15 MB (24.32%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& a, vector<vector<int>>& b) {
-    //   int i=0,j=0;
-    //   int m=a.size();
-    //   int n=b.size();
-    //   vector<vector<int>>  ans;
-    //   while(i<m && j<n){
-    //     if (a[i][0]<b[j][0]){
-    //         ans.push_back(a[i]);
-    //         i++;
+         vector<vector<int>> ans;
+         unordered_map<int,int> mp;
+         for(int i=0;i<a.size();i++){
+            int id=a[i][0];
+            int value=a[i][1];
 
-    //     }
-    //     else if (b[j][0]<a[i][0]){
-    //         ans.push_back(b[j]);
-    //         j++;
-    //     }
-    //     else{
-    //         ans.push_back({a[i][0],a[i][1]+b[j][1]});
-    //         i++;
-    //         j++;
-    //     }
-    //   } 
-    //   while(i<m) {
-    //      ans.push_back(a[i]);
-    //         i++;
-    //   } 
-    //   while(j<n){
-    //       ans.push_back(b[j]);
-    //         j++;
-    //   }
-    //    return ans; 
+            mp[id]+=value;
+         }
 
-vector<vector<int>>  ans;
-    unordered_map<int,int> mp;
-    int m=a.size();
-      int n=b.size();
-    for(int i=0;i<m;i++){
-        int id=a[i][0];
-        int value=a[i][1];
+         for(int i=0;i<b.size();i++){
+            int id=b[i][0];
+            int value=b[i][1];
 
-        mp[id]+=value;
-    }
+            mp[id]+=value;
+         }
 
-    for(int i=0;i<n;i++){
-         int id=b[i][0];
-        int value=b[i][1];
+         for(auto it:mp){
+            int id=it.first;
+            int val=it.second;
 
-        mp[id]+=value;
-    }
-    for(auto it:mp){
-        int key=it.first;
-        int value=it.second;
+            ans.push_back({id,val});
+         }
+         sort(ans.begin(),ans.end());
+         return ans;
 
-        ans.push_back({key,value});
-    }
-    sort(ans.begin(),ans.end());
-    return ans;
+
+        
     }
 };
