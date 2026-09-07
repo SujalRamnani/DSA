efff85458c7cab461d12a16dfc648d4c7e77cd0d
SujalRamnani <sujalramnani0@gmail.com>
2026-09-07 10:45:25 +0530

    Time: 0 ms (100%), Space: 15 MB (80.74%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& arr) {
         
+// int n=arr.size();
+// vector<int> ans(n);
+//         vector<int> leftSum(n,0);vector<int> rightSum(n,0);
+//         leftSum[0]=0;
+//         for(int i=1;i<arr.size();i++){
+//             leftSum[i]=leftSum[i-1]+arr[i-1];
+//         }
+//         rightSum[n-1]=0;
+//         for(int i=n-2;i>=0;i--){
+//             rightSum[i]=rightSum[i+1]+arr[i+1];
+//         }
+
+//         for(int i=0;i<n;i++){
+//             ans[i]=abs(leftSum[i]-rightSum[i]);
+//         }
+//         return ans;
+
+
+
 int n=arr.size();
-vector<int> ans(n);
-        vector<int> leftSum(n,0);vector<int> rightSum(n,0);
-        leftSum[0]=0;
-        for(int i=1;i<arr.size();i++){
-            leftSum[i]=leftSum[i-1]+arr[i-1];
-        }
+int totalSum=0;for(int i=0;i<n;i++) totalSum+=arr[i];
+vector<int> ans(n,0);
+int curr=0;
+for(int i=0;i<n;i++){
+    int leftSum=curr;
+    curr+=arr[i];
+    int rightSum=totalSum-curr;
+    ans[i]=abs(leftSum-rightSum);
 
-        rightSum[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            rightSum[i]=rightSum[i+1]+arr[i+1];
-        }
-
-        for(int i=0;i<n;i++){
-            ans[i]=abs(leftSum[i]-rightSum[i]);
-        }
-        return ans;
-        
+}
+    return ans;    
     }
 };
