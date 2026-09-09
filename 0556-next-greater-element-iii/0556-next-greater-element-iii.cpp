c520aed8eee83131072595aa2b1d2d833c0137d6
SujalRamnani <sujalramnani0@gmail.com>
2026-09-09 16:15:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.58%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        string s = to_string(n);
-
-        bool possible = next_permutation(s.begin(), s.end());
-
-        if (possible == false) {
-            return -1;
+        string s=to_string(n);
+        int saddle=-1;
+        for(int i=s.length()-2;i>=0;i--){
+            if (s[i]<s[i+1]){
+                ///saddle point mil gya
+                saddle=i;
+                break;
+            }
         }
-
-        long long ans = stoll(s);
-
-        if (ans > INT_MAX) {
-            return -1;
+        if (saddle==-1) return -1;
+      for(int i=s.length()-1;i>=0;i--){ ///just bada no than saddle point///
+        if (s[saddle]<s[i]){
+            swap(s[saddle],s[i]);
+            break;
         }
-
-        return ans;
+      }
+        reverse(s.begin()+saddle+1,s.end());
+        long long ans=stoll(s);
+        if(ans>INT_MAX) return -1;
+        return (int)ans;
     }
 };
