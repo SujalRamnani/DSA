6691111de800be4df0613cafcb6a071e1002c0ec
SujalRamnani <sujalramnani0@gmail.com>
2026-09-14 10:55:52 +0530

    Time: 3 ms (28.11%), Space: 14.8 MB (34.48%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,61 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& a, vector<vector<int>>& b) {
-        //  vector<vector<int>> ans;
-        //  unordered_map<int,int> mp;
-        //  for(int i=0;i<a.size();i++){
-        //     int id=a[i][0];
-        //     int value=a[i][1];
 
-        //     mp[id]+=value;
-        //  }
+         vector<vector<int>> ans;
+         int i=0,j=0;
+         int m=a.size(),n=b.size();
+         while(i<m && j<n){
+            int firstValue=a[i][0];
+            int secondValue=b[j][0];
 
-        //  for(int i=0;i<b.size();i++){
-        //     int id=b[i][0];
-        //     int value=b[i][1];
+            if (firstValue<secondValue){
+                 ans.push_back(a[i]);
+                 i++;
+            }
+            else if (firstValue>secondValue){
+                ans.push_back(b[j]);
+                j++;
+            }
+            else if (firstValue==secondValue){
+                ans.push_back({a[i][0],a[i][1]+b[j][1]});
+                i++;
+                j++;
+            }
+         }
 
-        //     mp[id]+=value;
-        //  }
+         while(i<m){
+           
+                 ans.push_back(a[i]);
+                 i++;
+            
+         }
 
-        //  for(auto it:mp){
-        //     int id=it.first;
-        //     int val=it.second;
-
-        //     ans.push_back({id,val});
-        //  }
-        //  sort(ans.begin(),ans.end());
-        //  return ans;
-
-
-vector<vector<int>> ans;
-int i=0,j=0;
-while(i<a.size() && j<b.size()){
-    if (a[i][0]<b[j][0]){
-        ans.push_back(a[i]);
-        i++;
-
-    }
-    else if (b[j][0]<a[i][0]){
-        ans.push_back(b[j]);
-        j++;
-    }
-    else if (a[i][0]==b[j][0]){
-        ans.push_back({a[i][0],a[i][1]+b[j][1]});
-        i++;
-        j++;
-    }
-}
-while(i<a.size()){
-    ans.push_back(a[i]);
-        i++;
-}
-while(j<b.size()){
-     ans.push_back(b[j]);
-        j++;
-}
-return ans;
-
-
-        
+         while(j<n){
+             ans.push_back(b[j]);
+                j++;
+         }
+         return ans;
     }
 };
