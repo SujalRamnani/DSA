9d64b72f0d76ecaa1640500e49a2ab94aea8227a
SujalRamnani <sujalramnani0@gmail.com>
2026-09-17 16:39:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& arr) {
+        ///pehle transpose for square matrix than reverse each row
 int m=arr.size();
 int n=arr[0].size();
 
-///pehle transpose fir reverse each row
-
-//transpose
 for(int i=0;i<m;i++){
+
     for(int j=0;j<i;j++){
         swap(arr[i][j],arr[j][i]);
     }
 }
-//abb reverse each row
 for(int i=0;i<m;i++){
     int a=0,b=n-1;
-    while(a<=b)
-    {
-     int temp=arr[i][a];
-     arr[i][a]=arr[i][b];
-     arr[i][b]=temp;
-     a++;
-     b--;
+    while(a<=b){
+        int temp=arr[i][a];
+        arr[i][a]=arr[i][b];
+        arr[i][b]=temp;
+        a++;b--;
     }
-
 }
 
     }
