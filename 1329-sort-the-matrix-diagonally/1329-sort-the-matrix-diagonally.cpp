4c832d68ca860494b4404d3ef13522f479dbc95c
SujalRamnani <sujalramnani0@gmail.com>
2026-09-18 09:45:08 +0530

    Time: 4 ms (34.18%), Space: 13.8 MB (16.89%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& arr) {
-        int m=arr.size();
-         int n=arr[0].size();
-        
-          unordered_map<int,vector<int>> mp;
-          for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[0].size();j++){
                 mp[i-j].push_back(arr[i][j]);
             }
-          }
-
-          for(auto &it:mp){
+        }
+        for(auto &it:mp){
             sort(it.second.begin(),it.second.end());
-          }
+        }
+     
+     for(int i=arr.size()-1;i>=0;i--){
+        for(int j=arr[0].size()-1;j>=0;j--){
+            arr[i][j]=mp[i-j].back();
+            mp[i-j].pop_back();
+        }
+     }
 
-          for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                arr[i][j]=mp[i-j].back();
-                mp[i-j].pop_back();
-            }
-          }
-return arr;
+        return arr;
     }
 };
