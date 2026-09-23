6434679dcf3767c72a838b9a31814dc2bb51cefc
SujalRamnani <sujalramnani0@gmail.com>
2026-09-23 09:51:26 +0530

    Time: 2 ms (2.73%), Space: 15.6 MB (82.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& arr) {
-        next_permutation(arr.begin(),arr.end());
+        // next_permutation(arr.begin(),arr.end());
+
+        int n=arr.size();
+        int pivotIndx=-1;
+        for(int i=n-2;i>=0;i--){
+            if (arr[i]<arr[i+1]){
+                pivotIndx=i;
+                break;
+            }
+        }
+        if (pivotIndx==-1){
+            reverse(arr.begin(),arr.end());
+            return ;
+        }
+
+        ///abb pivotIndx ke baad minimum jo bhi value hai uske saath arr[pivotIndx] ko swap karo///
+        for(int i=n-1;i>pivotIndx;i--){
+            if (arr[pivotIndx]<arr[i]){
+                swap(arr[pivotIndx],arr[i]);
+                break;
+            }
+        }
+        ///abb i ke baad wale ko reverse kardo aur array jo return kardo///
+        reverse(arr.begin()+pivotIndx+1,arr.end());
+        return;
+
+
+        
         
     }
 };
